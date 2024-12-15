5b4b277a41e8c5cfd9947f867fccfcf9603bbd0f
farahrguibirg <farahrguibi@gmail.com>
2024-12-15 21:57:52 +0100

    Update EmployeModel.java

--- Model/EmployeModel.java
+++ Model/EmployeModel.java
@@ -2,63 +2,58 @@ package Model;
 
 import java.util.List;
 
-import DAO.EmployeDAOImpl;
+import DAO.*;
 
 public class EmployeModel {
-    private EmployeDAOImpl dao;
-
-    public EmployeModel(EmployeDAOImpl dao) {
-        this.dao = dao;
+private EmployeDAOImpl dao;
+public EmployeModel(EmployeDAOImpl dao) {
+	this.dao=dao;}
+public boolean insertEmp(int id,String nom,String prenom,String email,String telephone ,Double salaire,Role role,Poste poste) {
+	 if (!email.contains("@") || !email.contains(".")) {
+         System.err.println("Email must be valid and contain '@' and '.' characters.");
+         return false;
+     }
+     if (telephone.length() != 10) {
+         System.err.println("The phone number must contain exactly 10 digits.");
+         return false;
+     }
+     Employe emp = new Employe(id,nom, prenom, email, telephone, salaire, role, poste);
+     dao.insertEmp(emp);
+     return true;
+ }
+public boolean updateEmp(int id,String nom,String prenom,String email,String telephone ,Double salaire,Role role,Poste poste) {
+	if (!email.contains("@") || !email.contains(".")) {
+        System.err.println("Email must be valid and contain '@' and '.' characters.");
+        return false;
     }
-
-    public boolean addEmploye(int id,String nom, String prenom, String email, String telephone, double salaire, Role role, Poste poste) {
-        if (!email.contains("@") || !email.contains(".")) {
-            System.err.println("Email must be valid and contain '@' and '.' characters.");
-            return false;
-        }
-        if (telephone.length() != 10) {
-            System.err.println("The phone number must contain exactly 10 digits.");
-            return false;
-        }
-        Employe emp = new Employe(id,nom, prenom, email, telephone, salaire, role, poste);
-        dao.addEmploye(emp);
-        return true;
+    if (telephone.length() != 10) {
+        System.err.println("The phone number must contain exactly 10 digits.");
+        return false;
     }
-
-    public boolean modifyEmploye(int id,String nom, String prenom, String email, String telephone, double salaire, Role role, Poste poste) {
-        if (!email.contains("@") || !email.contains(".")) {
-            System.err.println("Email must be valid and contain '@' and '.' characters.");
-            return false;
-        }
-        if (telephone.length() != 10) {
-            System.err.println("The phone number must contain exactly 10 digits.");
-            return false;
-        }
-        Employe emp = new Employe(id,nom, prenom, email, telephone, salaire, role, poste);
-        dao.modifyEmploye(emp);
-        return true;
-    }
-
-    public boolean deleteEmployee(int id){
-    	dao.deleteEmploye(id);
-    	return true;
-    }
-    public Object[][] getAllEmployees() {
-        List<Employe> employes = dao.getAllEmploye();
-        Object[][] employegs = new Object[employes.size()][8]; 
-
-        for (int i = 0; i < employes.size(); i++) {
-        	Employe emp = employes.get(i);
-        	employegs[i][0] = emp.getId();
-        	employegs[i][1] = emp.getNom();
-        	employegs[i][2] = emp.getPrenom();
-        	employegs[i][3] = emp.getEmail();
-        	employegs[i][4] = emp.getTelephone();
-        	employegs[i][5] = emp.getSalaire();
-        	employegs[i][6] = emp.getRole();
-        	employegs[i][7] = emp.getPoste();
-        }
-        return employegs;
-    }
+    Employe emp = new Employe(id,nom, prenom, email, telephone, salaire, role, poste);
+    dao.updateEmp(emp);
+    return true;
+}
+public boolean deleteEmp(int id){
+	dao.deleteEmp(id);
+	return true;
     
+}
+public Object[][] getAll() {
+    List<Employe> list = dao.getEmployes();
+    Object[][] employegs = new Object[list.size()][8]; 
+
+    for (int i = 0; i < list.size(); i++) {
+    	Employe emp = list.get(i);
+    	employegs[i][0] = emp.getId();
+    	employegs[i][1] = emp.getNom();
+    	employegs[i][2] = emp.getPrenom();
+    	employegs[i][3] = emp.getEmail();
+    	employegs[i][4] = emp.getTelephone();
+    	employegs[i][5] = emp.getSalaire();
+    	employegs[i][6] = emp.getRole();
+    	employegs[i][7] = emp.getPoste();
+    }
+    return employegs;
+}
 }
