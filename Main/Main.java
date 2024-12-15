74c8d54a0fada8a9a89cc98954980164a71cbf8f
farahrguibirg <farahrguibi@gmail.com>
2024-12-15 21:57:31 +0100

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -1,13 +1,16 @@
 package Main;
+
 import Model.*;
 import View.*;
 import DAO.*;
-import Controller.*;
+import Controller.EmployeController;
+
 public class Main {
-	public static void main(String[] args) {
-		EmployeDAOImpl dao = new EmployeDAOImpl();
-		EmployeModel model = new EmployeModel(dao);
-		EmployeView view = new EmployeView();
-		EmployeController controller = new EmployeController(view,model);
-	}
+    public static void main(String[] args) {
+        EmployeView view = new EmployeView();
+        EmployeDAOImpl dao = new EmployeDAOImpl();
+        EmployeModel model = new EmployeModel(dao);
+        new EmployeController(model, view);
+        view.setVisible(true);
+    }
 }
