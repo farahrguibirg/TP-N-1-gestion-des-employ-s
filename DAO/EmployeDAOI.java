06987b96e001f06ccddcbc3cb570a67dad77fbd2
farahrguibirg <farahrguibi@gmail.com>
2024-12-15 21:56:52 +0100

    Update EmployeDAOI.java

--- DAO/EmployeDAOI.java
+++ DAO/EmployeDAOI.java
@@ -1,11 +1,12 @@
 package DAO;
+
 import java.util.List;
 
 import Model.Employe;
+
 public interface EmployeDAOI {
-	public void addEmploye(Employe emp);
-	public void modifyEmploye(Employe emp);
-	public void deleteEmploye(int id);
-	public List<Employe> getAllEmploye();
-	
+public void insertEmp(Employe emp);
+public boolean deleteEmp(int id);
+public boolean updateEmp(Employe emp);
+public List<Employe> getEmployes();
 }
